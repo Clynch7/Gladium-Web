ae730474006c5948ef4ccff6f3b5357acaabdf53
Clynch7 <anton.silfver@gmail.com>
2019-03-08 21:19:35 +0100

    Basic flask running

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public/"));
 
 app.get('',function(req,res){
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/templates/index.html")
+});
+app.post('/createGladiator', function(req,res){
+
 });
 app.listen(HTTP_PORT,function(){
 })
